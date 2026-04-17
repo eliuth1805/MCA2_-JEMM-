9ac954d94785bd9b1cfdfc527fe51ad3460ca285
eliuth1805 <160825761+eliuth1805@users.noreply.github.com>
2026-04-16 20:10:38 -0600

    Implement projection matrix calculation from 3D to 2D
    Added code to compute the projection matrix from 3D to 2D points using SVD.

--- Tarea4 /dlt.py
+++ Tarea4 /dlt.py
@@ -1,1 +1,32 @@
+import numpy as np
 
+points_3D = np.array([
+    [0,0,10],
+    [6,0,10],
+    [0,4,10],
+    [6,4,10]
+])
+
+points_2D = np.array([
+    [0,0],
+    [1.2,0],
+    [0,0.8],
+    [1.2,0.8]
+])
+
+A = []
+
+for i in range(len(points_3D)):
+    X, Y, Z = points_3D[i]
+    x, y = points_2D[i]
+
+    A.append([X,Y,Z,1,0,0,0,0,-x*X,-x*Y,-x*Z,-x])
+    A.append([0,0,0,0,X,Y,Z,1,-y*X,-y*Y,-y*Z,-y])
+
+A = np.array(A)
+
+U, S, Vt = np.linalg.svd(A)
+P = Vt[-1].reshape(3,4)
+
+print("Projection matrix:")
+print(P)
